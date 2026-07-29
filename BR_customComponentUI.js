4e0a5da9363e39704a9d51c0deee14f0dfbf38e9
Shivendra Sharma <Shivendra.Sharma@proinception.com>
2026-07-29 13:39:51 +0530

    application status logic added for Rejected file

--- BR_customComponentUI.js
+++ BR_customComponentUI.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
                 else if (currentText == 'expiring soon' || currentText == 'pending approval' || currentText == 'under review by retal') {
                     $thisLabel.addClass('status-warn');
                 } 
-                else if (currentText == 'rejected') {
+                else if (currentText == 'rejected' || currentText == 'rejected by retal') {
                     $thisLabel.addClass('status-danger');
                 } 
                 else if (currentText == 'suspended' || currentText == 'e-sign pending') {
